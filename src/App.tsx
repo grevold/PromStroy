63d8fe499654815133e40577d35a27dd6d9844e2
Алексей <87669945+grevold@users.noreply.github.com>
2023-07-03 13:27:25 +0500

    Установка библиотек и начало проекта (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { MainPage } from "./pages/MainPage/MainPage";
+import { RoutePath } from "./types";
+
 function App() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route element={<MainPage />} path={RoutePath.MainPage} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,5 @@
+import s from "./MainPage.module.css";
+
+export function MainPage() {
+  return <div>Главная</div>;
+}
