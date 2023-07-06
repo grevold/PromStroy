4f897c72cf863885abab6092ad453b25c410ab1b
Алексей <87669945+grevold@users.noreply.github.com>
2023-07-06 17:07:28 +0300

    Рефакторинг блоков Клиенты (#9)

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,14 +1,14 @@
 import { ParalaxBanner } from "./components/ParalaxBanner/ParalaxBanner";
 import { History } from "./components/History/History";
 import { Industries } from "./components/Industries/Industries";
-
-import s from "./MainPage.module.css";
+import { ClientsBlock } from "./components/ClientsBlock/ClientsBlock";
 
 export function MainPage() {
   return (
     <div>
       <ParalaxBanner />
       <History />
+      <ClientsBlock />
       <Industries />
     </div>
   );
