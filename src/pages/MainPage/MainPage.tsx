784fe0b59a6dcd46d17698459b3c271bcb92b980
Алексей <87669945+grevold@users.noreply.github.com>
2023-07-12 17:10:12 +0300

    Добавил кнопки звонка и почты в модальное окно (#15)

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -16,6 +16,7 @@ export function MainPage() {
       <Industries />
       <Reward />
       <BlockFormEmail />
+      <Industries />
     </div>
   );
 }
--- src/pages/MainPage/components/Nav/components/NavMobile/components/ModalMenu/ModalMenu.tsx
+++ src/pages/MainPage/components/Nav/components/NavMobile/components/ModalMenu/ModalMenu.tsx
@@ -2,6 +2,9 @@ import { BurgerClose } from "../../../../../../../../icons/BurgerClose";
 import cn from "classnames";
 import ScrollLock from "react-scrolllock";
 import s from "./ModalMenu.module.css";
+import { Link } from "react-router-dom";
+import { EmailIcon } from "../../../../../../../../icons/EmailIcon";
+import { PhoneIcon } from "../../../../../../../../icons/PhoneIcon";
 
 interface Props {
   className?: string;
@@ -9,10 +12,42 @@ interface Props {
 }
 
 export function ModalMenu({ className, onClose }: Props) {
+  const scrollToEmail = () => {
+    onClose();
+    window.scroll({
+      top: 4880,
+      behavior: "smooth",
+    });
+  };
   return (
     <ScrollLock>
       <div className={cn(s.root, className)}>
         <BurgerClose onClose={onClose} className={s.burgerIconClose} />
+        <ul className={s.listLinks}>
+          <li>
+            <Link className={s.link} to="">
+              Главная
+            </Link>
+          </li>
+          <li>
+            <Link className={s.link} to="">
+              Галерея
+            </Link>
+          </li>
+          <li>
+            <Link className={s.link} to="">
+              Контакты
+            </Link>
+          </li>
+          <li className={s.blockFeedback}>
+            <button className={s.circle}>
+              <EmailIcon onClose={scrollToEmail} />
+            </button>
+            <a className={s.circle} href="tel:+79001111111">
+              <PhoneIcon />
+            </a>
+          </li>
+        </ul>
       </div>
     </ScrollLock>
   );
