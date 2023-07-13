7cd1e9d08ec3a8312f28238799b14fabfed71972
Алексей <87669945+grevold@users.noreply.github.com>
2023-07-13 16:02:29 +0300

    Добавил анимацию для модального окна (#16)

--- src/pages/MainPage/components/Nav/components/NavMobile/useNavMobile.ts
+++ src/pages/MainPage/components/Nav/components/NavMobile/useNavMobile.ts
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 export function useNavMobile() {
   const [state, setState] = useState(false);
-  const handleClick = () => setState((prevState) => !prevState);
+  function handleClick() {
+    setState((prevState) => !prevState);
+  }
   return { state, handleClick };
 }
