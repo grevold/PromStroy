f3c286bacda6c1136d8b5b7761eb71a481d9d6bd
Alexey Grevtsov <grevold@yandex.ru>
2023-07-19 23:31:32 +0300

    Реализовал маску для ввода номера телефона

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
-import { MainPage } from "./pages/MainPage/MainPage";
 import { RoutePath } from "./types";
-import { ContactsPage } from "./pages/ContactsPage/ContactsPage";
-import { RequisitesPage } from "./pages/RequisitesPage/RequisitesPage";
 import { ScrollToTop } from "./components/ScrollToTop/ScrollToTop";
+import { ContactsPage } from "./pages/ContactsPage/ContactsPage";
+import { MainPage } from "./pages/MainPage/MainPage";
+import { RequisitesPage } from "./pages/RequisitesPage/RequisitesPage";
 
 function App() {
   return (
