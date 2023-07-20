baf5d29a8aa4185cf793b5521b7c5282af18bf2e
Алексей <87669945+grevold@users.noreply.github.com>
2023-07-20 13:15:39 +0300

    Внес изменения от заказчика (#28)

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -4,7 +4,6 @@ import { Industries } from "./components/Industries/Industries";
 import { ClientsBlock } from "./components/ClientsBlock/ClientsBlock";
 import { Reward } from "./components/Reward/Reward";
 import { BlockFormEmail } from "./components/FormEmail/BlockFormEmail";
-import { Requisites } from "./components/Requisites/Requisites";
 import { Nav } from "../../components/Nav/Nav";
 import { Footer } from "../../components/Footer/Footer";
 
@@ -17,7 +16,6 @@ export function MainPage() {
       <ClientsBlock />
       <Industries />
       <Reward />
-      <Requisites />
       <BlockFormEmail />
       <Footer />
     </div>
